e12a1d6a1ad8544223c241987e017ad884c5ef3c
Julian <jperelli@gmail.com>
2017-03-27 16:55:44 -0300

    refs #3 mejorando placeholders de links a listados de casos en la portada

--- src/components/Index-content.vue
+++ src/components/Index-content.vue
@@ -6,37 +6,37 @@
 
         <div class="md-width-1of2 bg-width-1of2 lg-width-1of3">
           <router-link to="/casos/?issue.type=Encontrado">
-            <img src="http://placehold.it/250x150">
+            <img src="http://placehold.it/250x150?text=Encontrado">
           </router-link>
         </div>
 
         <div class="md-width-1of2 bg-width-1of2 lg-width-1of3">
           <router-link to="/casos/?issue.type=Perdido">
-            <img src="http://placehold.it/250x150">
+            <img src="http://placehold.it/250x150?text=Perdido">
           </router-link>
         </div>
 
         <div class="md-width-1of2 bg-width-1of2 lg-width-1of3">
           <router-link to="/casos/?issue.type=Traslado">
-            <img src="http://placehold.it/250x150">
+            <img src="http://placehold.it/250x150?text=Traslado">
           </router-link>
         </div>
 
         <div class="md-width-1of2 bg-width-1of2 lg-width-1of3">
           <router-link to="/casos/?issue.type=Tránsito">
-            <img src="http://placehold.it/250x150">
+            <img src="http://placehold.it/250x150?text=Tránsito">
           </router-link>
         </div>
 
         <div class="md-width-1of2 bg-width-1of2 lg-width-1of3">
           <router-link to="/casos/?issue.type=Adopción">
-            <img src="http://placehold.it/250x150">
+            <img src="http://placehold.it/250x150?text=Adoción">
           </router-link>
         </div>
 
         <div class="md-width-1of2 bg-width-1of2 lg-width-1of3">
           <router-link to="/casos/?issue.type=Colaboración Económica">
-            <img src="http://placehold.it/250x150">
+            <img src="http://placehold.it/250x150?text=Colaboración Económica">
           </router-link>
         </div>
 
